8459261def668b0716581477463ddaada32a18a7
Monika Marciniak <monixxa90@gmail.com>
2022-07-18 23:27:56 +0200

    changes

--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,10 @@
+const button = document.querySelector("button.change-color");
+const colorName = document.querySelector("div.widget>p>span.color");
+const body = document.querySelector("body");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const button = document.querySelector(".change-color");
-
-button.addEventListener("click");
+button.addEventListener("click", e => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorName.textContent = `${getRandomHexColor()}`;
+});
